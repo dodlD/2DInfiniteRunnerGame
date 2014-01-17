b4a4d0d64e31936a5a9d5f9ddca574e673907730
LKPIT11DlJo <LKPIT11DlJo@LKPIT11DLJO.itg-larande.local>
2014-01-17 15:46:29 +0100

    Still working on the collision

--- src/irgame/physics/Collision.java
+++ src/irgame/physics/Collision.java
@@ -43,15 +43,22 @@ public class Collision {
                     Game.chaR.xPos -= Game.chaR.HORIZ_VEL;
                 }
         }*/
-        
+        System.out.println(Game.chaR.state);
         for (int i = 0; i < Game.ground.length; i++){     
             if (Game.chaR.Intersect(Game.ground[i].hitBox)){
                 if (Game.chaR.xPos + Game.chaR.WIDTH >= Game.ground[i].xPos && Game.ground[i].yPos < Game.HEIGHT - 32){
                     if (Game.chaR.state.equals("jumping")){
                         Game.chaR.xPos -= Game.chaR.HORIZ_VEL;                      
-                    }else{
+                    }else if (Game.chaR.state.equals("falling")){ 
+                        if (Game.chaR.yPos + 64 > Game.ground[i].yPos && Game.chaR.yPos + 64 < Game.ground[i].yPos + 32){
+                            Game.chaR.xPos -= Game.chaR.HORIZ_VEL;
+                            Game.chaR.yPos += Game.gravity;
+                        }
                         Game.chaR.yPos -= Game.gravity;
-                        Game.chaR.xPos -= Game.chaR.HORIZ_VEL;  
+                    }else {
+                        Game.chaR.xPos -= Game.chaR.HORIZ_VEL; 
+                        Game.chaR.yPos -= Game.gravity;
+                        System.out.println("yolo");
                     }
                 }else {
                     Game.chaR.yPos -= Game.gravity;
